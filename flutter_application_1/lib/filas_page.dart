0d53fe6dadf8ad82a565e4d0cc2a47d97a32f5dd
VitorLaboissiere <va090178@gmail.com>
2025-03-27 15:59:48 -0300

    Atualzação das Filas

--- flutter_application_1/lib/filas_page.dart
+++ flutter_application_1/lib/filas_page.dart
@@ -1,40 +1,41 @@
 import 'package:flutter/material.dart';
 
-// ignore: use_key_in_widget_constructors
 class FilasPage extends StatefulWidget {
+  const FilasPage({Key? key}) : super(key: key);
+
   @override
-  // ignore: library_private_types_in_public_api
-  _FilasPageState createState() => _FilasPageState();
+  State<FilasPage> createState() => FilasPageState();
 }
 
-class _FilasPageState extends State<FilasPage> {
+class FilasPageState extends State<FilasPage> {
   final Map<String, TextEditingController> _controllers = {
     'Fila Agendamento:': TextEditingController(),
     'Fila Aniversariante:': TextEditingController(),
-    'Fila Pós atendimento:': TextEditingController(),
+    'Fila Pós Atendimento:': TextEditingController(),
     'Fila Avaliação:': TextEditingController(),
     'Fila Cobrança:': TextEditingController(),
-    'Fila Re-Cobrança: ': TextEditingController(),
+    'Fila Re-Cobrança:': TextEditingController(),
     'Fila Confirmação:': TextEditingController(),
   };
 
-  final Map<String, FocusNode> _focusNodes = {}; // Mapa para armazenar o foco de cada caixa
+  final Map<String, FocusNode> _focusNodes = {};
 
   @override
   void initState() {
     super.initState();
-    // Inicializando o FocusNode para cada campo
-    _controllers.forEach((key, value) {
+    for (final key in _controllers.keys) {
       _focusNodes[key] = FocusNode();
-    });
+    }
   }
 
   @override
   void dispose() {
-    // ignore: avoid_function_literals_in_foreach_calls
-    _controllers.values.forEach((controller) => controller.dispose());
-    // ignore: avoid_function_literals_in_foreach_calls
-    _focusNodes.values.forEach((focusNode) => focusNode.dispose());
+    for (final controller in _controllers.values) {
+      controller.dispose();
+    }
+    for (final focusNode in _focusNodes.values) {
+      focusNode.dispose();
+    }
     super.dispose();
   }
 
@@ -53,56 +54,70 @@ class _FilasPageState extends State<FilasPage> {
           children: [
             const Text(
               'Configure as filas:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
+            // Column para alinhar as caixas de mensagem uma embaixo da outra
             Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 2, // Menor espaço entre as colunas
-                mainAxisSpacing: 2, // Menor espaço entre as linhas
-                childAspectRatio: 15, // Caixa mais compacta
+              child: ListView(
                 children: _controllers.keys.map((label) {
-                  return GestureDetector(
-                    onTap: () {
-                      // Quando o usuário clica na caixa, o foco é atribuído ao campo de texto correspondente
-                      FocusScope.of(context).requestFocus(_focusNodes[label]);
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(4), // Menos padding para deixar menor
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              label,
-                              style: const TextStyle(fontSize: 18, color: Colors.white), // Texto menor
-                            ),
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 6),
+                    child: Row(
+                      children: [
+                        // Rótulo da fila
+                        SizedBox(
+                          width: 200, // Define o tamanho fixo para o texto
+                          child: Text(
+                            label,
+                            style: const TextStyle(fontSize: 16, color: Colors.white),
                           ),
-                          const SizedBox(width: 4), // Menor espaço entre o texto e o campo de entrada
-                          SizedBox(
-                            width: 3, // Caixa de texto menor
-                            child: TextField(
-                              controller: _controllers[label],
-                              focusNode: _focusNodes[label], // Associando o FocusNode ao campo de texto
-                              keyboardType: TextInputType.number,
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                contentPadding: EdgeInsets.symmetric(horizontal: 4), // Menor padding dentro da caixa de texto
+                        ),
+                        const SizedBox(width: 6), // Espaço entre o texto e o campo de entrada
+                        
+                        // Caixa de texto compacta
+                        SizedBox(
+                          width: 35, // Define o tamanho fixo para o campo de texto
+                          child: TextField(
+                            controller: _controllers[label],
+                            focusNode: _focusNodes[label],
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white.withOpacity(0.2),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(6.0),
+                                borderSide: const BorderSide(
+                                  color: Colors.white,
+                                  width: 0.5,
+                                ),
                               ),
-                              style: const TextStyle(color: Colors.white),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(6.0),
+                                borderSide: const BorderSide(
+                                  color: Colors.white,
+                                  width: 0.5,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(6.0),
+                                borderSide: const BorderSide(
+                                  color: Colors.white,
+                                  width: 1.0,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
                             ),
+                            style: const TextStyle(color: Colors.white),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   );
                 }).toList(),
               ),
             ),
+            const SizedBox(height: 16),
             Center(
               child: ElevatedButton(
                 onPressed: () {
@@ -111,7 +126,7 @@ class _FilasPageState extends State<FilasPage> {
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
-                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12), // Botão menor
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8),
                   ),
