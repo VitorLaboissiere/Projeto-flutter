e25ba73c9e99080d6150b2728c5cbf8ebeb96547
VitorLaboissiere <va090178@gmail.com>
2025-05-26 10:21:29 -0300

    Atualização buton

--- flutter_application_1/Flutter_app/lib/services/api_service.dart
+++ flutter_application_1/Flutter_app/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String _baseUrl = 'http://localhost:5123/api/mensageria'; // coloque o IP se for real (ex: 192.168.0.100)
+  static const String _baseUrl = '/int/enqueueMessageToSend '; // coloque o IP se for real (ex: 192.168.0.100)
 
   static const String _apiKey = 'SUA_CHAVE_AQUI';
 
@@ -21,6 +21,22 @@ class ApiService {
       throw Exception('Erro ao carregar mensagens');
     }
   }
+ // Aqui você pode adicionar POST, PUT, DELETE, etc.
+static Future<void> enviarMensagem(String parametro, String conteudo) async {
+  final response = await http.post(
+    Uri.parse("$_baseUrl/mensagem"),
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": _apiKey,
+    },
+    body: jsonEncode({
+      "parametro": parametro,
+      "conteudo": conteudo,
+    }),
+  );
 
-  // Aqui você pode adicionar POST, PUT, DELETE, etc.
+  if (response.statusCode != 201) {
+    throw Exception('Erro ao enviar mensagem: ${response.body}');
+  }
+}
 }
